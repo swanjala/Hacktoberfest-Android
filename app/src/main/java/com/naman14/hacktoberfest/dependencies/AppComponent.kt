0c956db306633001c4f0044d71682e77f581388e
live-beta <wanjala.sam.s@gmail.com>
2019-09-26 18:39:54 +0300

    (add): app component
    - Inject hacktober application

--- app/src/main/java/com/naman14/hacktoberfest/dependencies/AppComponent.kt
+++ app/src/main/java/com/naman14/hacktoberfest/dependencies/AppComponent.kt
@@ -1,4 +1,9 @@
 package com.naman14.hacktoberfest.dependencies
 
+import com.naman14.hacktoberfest.application.HacktoberApplication
+
 interface AppComponent {
+
+    fun inject(target:HacktoberApplication)
+
 }
