c28c36c76d97a624819979e75c87158ad16dbdfb
live-beta <wanjala.sam.s@gmail.com>
2019-09-26 14:05:00 +0300

    (add): network service provider

--- app/src/main/java/com/naman14/hacktoberfest/dependencies/AppModule.kt
+++ app/src/main/java/com/naman14/hacktoberfest/dependencies/AppModule.kt
@@ -2,8 +2,10 @@ package com.naman14.hacktoberfest.dependencies
 
 import android.app.Application
 import android.content.Context
+import com.naman14.hacktoberfest.network.api.services.SearchService
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
@@ -11,5 +13,11 @@ class AppModule( private val application: Application){
     @Provides
     @Singleton
     fun provideContext(): Context = application
+
+    @Provides
+    @Singleton
+    fun provideNetworkService(retrofit: Retrofit): SearchService{
+        return retrofit.create(SearchService::class.java)
+    }
 
 }
